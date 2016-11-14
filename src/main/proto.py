358836e5f02c6e7ee8735b58d5bf3b388443082a
Daniel Zhang <dzhang@hmc.edu>
2016-11-14 01:56:11 -0800

    created basic prototype

---
+++ src/main/proto.py
@@ -0,0 +1,27 @@
+import os
+from diregex_semantics import Matcher
+from diregex_parser import parse
+import diregex_lexer
+from diregex_ir import *
+
+print(os.getcwd())
+os.chdir('../test/testdir')
+
+def match(diregex):
+    ast = parse(diregex)
+
+    matcher = Matcher()
+    matches = matcher.visit(ast, os.getcwd())
+    for match in matches:
+        print(match)
+
+
+print("----Example 1------")
+match("\"parent\"")
+
+print("----Example 2------")
+match("\"[a-z]*\"/\"[a-z0-9]*\"")
+
+print("----Example 3------")
+match("\"parent\"/(\"ch[a-z0-9]*\", \"[a-z0-9]*\")")
+
