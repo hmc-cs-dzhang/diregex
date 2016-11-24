af920c27a304a210f268a6815919d6652a993642
Daniel Zhang <dzhang@hmc.edu>
2016-11-23 20:00:50 -0800

    added main program

---
+++ src/main/diregex.py
@@ -0,0 +1,18 @@
+import diregex_exec
+
+def main():
+    diregex = input("enter diregex: ")
+    command = ''
+    line = input("enter command: ")
+    while line != '':
+        command += line + '\n'
+        line = input("enter command: ")
+
+    diregex_exec.test(diregex, command)
+
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
